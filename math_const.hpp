be5968d12eb9612a4db8f6469362ed5f8ada73c7
markdellostritto <43013452+markdellostritto@users.noreply.github.com>
2019-09-20 18:59:29 -0400

    added log2

--- math_const.hpp
+++ math_const.hpp
@@ -1,13 +1,16 @@
+#pragma once
 #ifndef MATH_CONST_HPP
 #define MATH_CONST_HPP
 
 namespace num_const{
-	static const double ZERO=1e-6;//small number for float comparison
-	static const double PI=3.141592653589793;//pi
-	static const double RadPI=1.772453850905516;//radical pi
-	static const double Rad2=1.41421356237310;//radical 2
-	static const double E=2.71828182845905;//euler's constant
-	static const double PHI=1.6180339887499;//golden ratio
+	static const double ZERO  =1e-6;//small number for float comparison
+	static const double PI    =3.141592653589793;//pi
+	static const double RadPI =1.772453850905516;//radical pi
+	static const double Rad2  =1.414213562373095;//radical 2
+	static const double Rad3  =1.732050807568877;//radical 3
+	static const double E     =2.718281828459045;//euler's constant
+	static const double PHI   =1.618033988749895;//golden ratio
+	static const double LOG2  =0.693147180559945;//Log[2,E]
 }
 
 #endif
