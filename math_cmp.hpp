b934556cfa4e310c00f45e2e5b4cc6fff111c4b2
markdellostritto <43013452+markdellostritto@users.noreply.github.com>
2019-09-20 18:56:51 -0400

    made functions inline and noexcept

--- math_cmp.hpp
+++ math_cmp.hpp
@@ -1,11 +1,12 @@
+#pragma once
 #ifndef MATH_CMP_HPP
 #define MATH_CMP_HPP
 
 namespace cmp{
 	
-	template <class T> T max(T x1, T x2){return (x1>x2)?x1:x2;}
-	template <class T> T min(T x1, T x2){return (x1>x2)?x2:x1;}
-	template <class T> unsigned int delta(const T& x1, const T& x2){return (x1==x2);}
+	template <class T> inline T max(T x1, T x2)noexcept{return (x1>x2)?x1:x2;}
+	template <class T> inline T min(T x1, T x2)noexcept{return (x1>x2)?x2:x1;}
+	template <class T> inline unsigned int delta(const T& x1, const T& x2)noexcept{return (x1==x2);}
 	
 };
 
