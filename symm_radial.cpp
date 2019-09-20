8070594ab15be224df8a52c014159d15236c23ee
markdellostritto <43013452+markdellostritto@users.noreply.github.com>
2019-09-20 19:04:27 -0400

    added support for "tanh" symmetry functions

--- symm_radial.cpp
+++ symm_radial.cpp
@@ -1,23 +1,33 @@
 #include "symm_radial.hpp"
 
+//*****************************************
+// PhiRN - radial function names
+//*****************************************
+
 //loading/printing
 
 PhiRN::type PhiRN::load(const char* str){
 	if(std::strcmp(str,"G1")==0) return PhiRN::G1;
 	else if(std::strcmp(str,"G2")==0) return PhiRN::G2;
+	else if(std::strcmp(str,"T1")==0) return PhiRN::T1;
 	else return PhiRN::UNKNOWN;
 }
 
 std::ostream& operator<<(std::ostream& out, const PhiRN::type& t){
 	if(t==PhiRN::G1) out<<"G1";
 	else if(t==PhiRN::G2) out<<"G2";
+	else if(t==PhiRN::T1) out<<"T1";
 	else out<<"UNKNOWN";
 	return out;
 }
 
 //PhiR
 std::ostream& operator<<(std::ostream& out, const PhiR& f){
-	return out<<"PhiR "<<f.tcut<<" "<<f.rc;
+	return out;
+}
+
+bool operator==(const PhiR& phir1, const PhiR& phir2){
+	return true;
 }
 
 namespace serialize{
@@ -27,10 +37,7 @@ namespace serialize{
 	//**********************************************
 	
 	template <> unsigned int nbytes(const PhiR& obj){
-		unsigned int N=0;
-		N+=sizeof(double);//rc
-		N+=sizeof(obj.tcut);//cutoff type
-		return N;
+		return 0;
 	}
 	
 	//**********************************************
@@ -38,8 +45,7 @@ namespace serialize{
 	//**********************************************
 	
 	template <> void pack(const PhiR& obj, char* arr){
-		std::memcpy(arr,&obj.rc,sizeof(double));
-		std::memcpy(arr+sizeof(double),&obj.tcut,sizeof(obj.tcut));
+		
 	}
 	
 	//**********************************************
@@ -47,8 +53,7 @@ namespace serialize{
 	//**********************************************
 	
 	template <> void unpack(PhiR& obj, const char* arr){
-		std::memcpy(&obj.rc,arr,sizeof(double));
-		std::memcpy(&obj.tcut,arr+sizeof(double),sizeof(obj.tcut));
+		
 	}
 	
 }
--- symm_radial.hpp
+++ symm_radial.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #ifndef SYMM_RADIAL_HPP
 #define SYMM_RADIAL_HPP
 
@@ -10,29 +11,33 @@
 // local libraries - serialization
 #include "serialize.hpp"
 
-//radial function names
+//*****************************************
+// PhiRN - radial function names
+//*****************************************
+
 struct PhiRN{
 	enum type{
 		UNKNOWN=-1,
 		G1=0,//Behler G1
-		G2=1//Behler G2
+		G2=1,//Behler G2
+		T1=2//tanh
 	};
 	static type load(const char* str);
 };
 std::ostream& operator<<(std::ostream& out, const PhiRN::type& t);
 
-//PhiR
+//*****************************************
+// PhiR - radial function base class
+//*****************************************
+
 struct PhiR{
-	double rc;//cutoff radius
-	CutoffN::type tcut;//cutoff function type
-	PhiR():rc(0),tcut(CutoffN::COS){};
-	PhiR(CutoffN::type tcut_, double rc_):tcut(tcut_),rc(rc_){};
 	virtual ~PhiR(){};
-	virtual double operator()(double r)const noexcept=0;
-	virtual double val(double r)const noexcept=0;
-	virtual double grad(double r)const noexcept=0;
+	virtual double val(double r, double cut)const noexcept=0;
+	virtual double grad(double r, double cut, double gcut)const noexcept=0;
 };
 std::ostream& operator<<(std::ostream& out, const PhiR& f);
+bool operator==(const PhiR& phir1, const PhiR& phir2);
+inline bool operator!=(const PhiR& phir1, const PhiR& phir2){return !(phir1==phir2);};
 
 namespace serialize{
 	
