8201ea29d6f3f733dae8446539e501afdc664e23
markdellostritto <43013452+markdellostritto@users.noreply.github.com>
2019-09-20 18:58:41 -0400

    optimized loops

--- map.cpp
+++ map.cpp
@@ -9,8 +9,15 @@ namespace serialize{
 	template <> unsigned int nbytes(const Map<std::string,unsigned int>& obj){
 		unsigned int size=0;
 		size+=sizeof(unsigned int);//size
-		size+=obj.size()*sizeof(unsigned int);//value
 		for(unsigned int i=0; i<obj.size(); ++i) size+=nbytes(obj.key(i));//key
+		size+=obj.size()*sizeof(unsigned int);//value
+		return size;
+	}
+	template <> unsigned int nbytes(const Map<unsigned int,unsigned int>& obj){
+		unsigned int size=0;
+		size+=sizeof(unsigned int);//size
+		size+=obj.size()*sizeof(unsigned int);//key
+		size+=obj.size()*sizeof(unsigned int);//value
 		return size;
 	}
 	
@@ -20,11 +27,28 @@ namespace serialize{
 	
 	template <> void pack(const Map<std::string,unsigned int>& obj, char* arr){
 		unsigned int pos=0;
-		unsigned int size=obj.size();
+		//size
+		const unsigned int size=obj.size();
 		std::memcpy(arr+pos,&size,sizeof(unsigned int)); pos+=sizeof(unsigned int);
+		//key
 		for(unsigned int i=0; i<size; ++i){
 			pack(obj.key(i),arr+pos); pos+=nbytes(obj.key(i));
 		}
+		//val
+		for(unsigned int i=0; i<size; ++i){
+			std::memcpy(arr+pos,&obj.val(i),sizeof(unsigned int)); pos+=sizeof(unsigned int);
+		}
+	}
+	template <> void pack(const Map<unsigned int,unsigned int>& obj, char* arr){
+		unsigned int pos=0;
+		//size
+		const unsigned int size=obj.size();
+		std::memcpy(arr+pos,&size,sizeof(unsigned int)); pos+=sizeof(unsigned int);
+		//key
+		for(unsigned int i=0; i<size; ++i){
+			std::memcpy(arr+pos,&obj.key(i),sizeof(unsigned int)); pos+=sizeof(unsigned int);
+		}
+		//val
 		for(unsigned int i=0; i<size; ++i){
 			std::memcpy(arr+pos,&obj.val(i),sizeof(unsigned int)); pos+=sizeof(unsigned int);
 		}
@@ -37,16 +61,41 @@ namespace serialize{
 	template <> void unpack(Map<std::string,unsigned int>& obj, const char* arr){
 		unsigned int pos=0;
 		obj.clear();
+		//size
 		unsigned int size=0;
 		std::memcpy(&size,arr+pos,sizeof(unsigned int)); pos+=sizeof(unsigned int);
 		std::vector<std::string> key(size);
 		std::vector<unsigned int> val(size);
+		//key
 		for(unsigned int i=0; i<size; ++i){
 			unpack(key[i],arr+pos); pos+=nbytes(key[i]);
 		}
+		//val
 		for(unsigned int i=0; i<size; ++i){
 			std::memcpy(&val[i],arr+pos,sizeof(unsigned int)); pos+=sizeof(unsigned int);
 		}
+		//assign
+		for(unsigned int i=0; i<size; ++i){
+			obj.add(key[i],val[i]);
+		}
+	}
+	template <> void unpack(Map<unsigned int,unsigned int>& obj, const char* arr){
+		unsigned int pos=0;
+		obj.clear();
+		//size
+		unsigned int size=0;
+		std::memcpy(&size,arr+pos,sizeof(unsigned int)); pos+=sizeof(unsigned int);
+		std::vector<unsigned int> key(size);
+		std::vector<unsigned int> val(size);
+		//key
+		for(unsigned int i=0; i<size; ++i){
+			std::memcpy(&key[i],arr+pos,sizeof(unsigned int)); pos+=sizeof(unsigned int);
+		}
+		//val
+		for(unsigned int i=0; i<size; ++i){
+			std::memcpy(&val[i],arr+pos,sizeof(unsigned int)); pos+=sizeof(unsigned int);
+		}
+		//assign
 		for(unsigned int i=0; i<size; ++i){
 			obj.add(key[i],val[i]);
 		}
--- map.hpp
+++ map.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #ifndef MAP_HPP
 #define MAP_HPP
 
@@ -13,17 +14,17 @@ private:
 	std::vector<T2> val_;
 public:
 	//constructors/destructors
-	Map(){};
-	~Map(){};
+	Map(){}
+	~Map(){}
 	
 	//access
-	const std::vector<T1>& key()const{return key_;};
-	const T1& key(unsigned int i)const{return key_[i];};
-	const std::vector<T2>& val()const{return val_;};
-	const T2& val(unsigned int i)const{return val_[i];};
+	const std::vector<T1>& key()const{return key_;}
+	const T1& key(unsigned int i)const{return key_[i];}
+	const std::vector<T2>& val()const{return val_;}
+	const T2& val(unsigned int i)const{return val_[i];}
 	
 	//member functions
-	unsigned int size()const{return key_.size();};
+	unsigned int size()const{return key_.size();}
 	void add(const T1& k, const T2& v);
 	void remove(const T1& k);
 	bool find(const T1& k)const;
@@ -42,7 +43,7 @@ void Map<T1,T2>::add(const T1& k, const T2& v){
 template <class T1, class T2>
 void Map<T1,T2>::remove(const T1& k){
 	int index=-1;
-	for(unsigned int i=0; i<key_.size(); ++i){
+	for(int i=key_.size()-1; i>=0; --i){
 		if(k==key_[i]){index=i;break;}
 	}
 	if(index>=0){
@@ -54,7 +55,7 @@ void Map<T1,T2>::remove(const T1& k){
 template <class T1, class T2>
 bool Map<T1,T2>::find(const T1& k)const{
 	int index=-1;
-	for(unsigned int i=0; i<key_.size(); ++i){
+	for(int i=key_.size()-1; i>=0; --i){
 		if(k==key_[i]){index=i;break;}
 	}
 	if(index<0) return false;
@@ -70,7 +71,7 @@ void Map<T1,T2>::clear(){
 template <class T1, class T2>
 const T2& Map<T1,T2>::operator[](const T1& k)const{
 	int index=-1;
-	for(unsigned int i=0; i<key_.size(); ++i){
+	for(int i=key_.size()-1; i>=0; --i){
 		if(k==key_[i]){index=i;break;}
 	}
 	if(index<0) throw std::runtime_error("No match for key in map");
@@ -80,7 +81,7 @@ const T2& Map<T1,T2>::operator[](const T1& k)const{
 template <class T1, class T2>
 bool operator==(const Map<T1,T2>& map1, const Map<T1,T2>& map2){
 	if(map1.size()!=map2.size()) return false;
-	for(unsigned int i=0; i<map1.size(); ++i){
+	for(int i=map1.size()-1; i>=0; --i){
 		if(map1.val(i)!=map2[map1.key(i)]) return false;
 	}
 	return true;
@@ -98,18 +99,21 @@ namespace serialize{
 	//**********************************************
 	
 	template <> unsigned int nbytes(const Map<std::string,unsigned int>& obj);
+	template <> unsigned int nbytes(const Map<unsigned int,unsigned int>& obj);
 	
 	//**********************************************
 	// packing
 	//**********************************************
 	
 	template <> void pack(const Map<std::string,unsigned int>& obj, char* arr);
+	template <> void pack(const Map<unsigned int,unsigned int>& obj, char* arr);
 	
 	//**********************************************
 	// unpacking
 	//**********************************************
 	
 	template <> void unpack(Map<std::string,unsigned int>& obj, const char* arr);
+	template <> void unpack(Map<unsigned int,unsigned int>& obj, const char* arr);
 	
 }
 
