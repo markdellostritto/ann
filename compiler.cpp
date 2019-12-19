50ec25fddbaf5fa5b2a6b12079a5288d93f23e08
markdellostritto <43013452+markdellostritto@users.noreply.github.com>
2019-12-18 19:29:55 -0500

    added more functions for compiler info

--- compiler.cpp
+++ compiler.cpp
@@ -1,3 +1,4 @@
+// ann - compiler
 #include "compiler.hpp"
 
 namespace compiler{
@@ -14,7 +15,7 @@ std::string standard(){
 	return str;
 }
 
-std::string version(){
+std::string name(){
 	std::string str;
 	//clang
 	#if defined __clang__
@@ -37,12 +38,71 @@ std::string version(){
 	return str;
 }
 
-std::string instruction(){
+std::string version(){
+	std::string str(__VERSION__);
+	return str;
+}
+
+std::string date(){
+	std::string str(__DATE__);
+	return str;
+}
+
+std::string time(){
+	std::string str(__TIME__);
+	return str;
+}
+
+std::string arch(){
+	std::string str;
+	#ifdef defined __i386__
+		str="i386"
+	#elif defined __i486__
+		str="i486"
+	#elif defined __i586__
+		str="i586"
+	#elif defined __i686__
+		str="i686"
+	#elif defined __arm__
+		str="arm";
+	#elif defined __aarch64__
+		str="arm64";
+	#elif (defined(__amd64__) || defined(__x86_64__))
+		str="amd64";
+	#endif
+	return str;
+}
+
+std::string instr(){
 	std::string str;
 	#if defined __AVX__
 		str="AVX";
 	#elif defined __AVX2__
 		str="AVX2";
+	#endif
+	return str;
+}
+
+std::string omp(){
+	std::string str="FALSE";
+	#ifdef _OPENMP
+		str="TRUE";
+	#endif
+	return str;
+}
+
+std::string os(){
+	std::string str;
+	#if (defined(__WIN32) || defined(__WIN64))
+		str="windows";
+	#elif defined __CYGWIN__
+		str="cygwin";
+	#elif (defined(__APPLE__) || defined(__MACH__))
+		str="mac";
+	#elif defined __linux__
+		str="linux";
+	#elif defined __unix__
+		str="unix";
 	#endif
 	return str;
 }
--- compiler.hpp
+++ compiler.hpp
@@ -2,13 +2,20 @@
 #ifndef COMPILER_HPP
 #define COMPILER_HPP
 
+// c++ libraries
 #include <string>
 
 namespace compiler{
 
 	std::string standard();
+	std::string name();
 	std::string version();
-	std::string instruction();
+	std::string date();
+	std::string time();
+	std::string arch();
+	std::string instr();
+	std::string omp();
+	std::string os();
 	
 }
 
