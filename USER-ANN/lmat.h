d497bf6c785551c6b2426080cbcda71c8b0db5f9
markdellostritto <43013452+markdellostritto@users.noreply.github.com>
2019-12-18 20:00:24 -0500

    added const where appropriate

--- USER-ANN/lmat.h
+++ USER-ANN/lmat.h
@@ -1,56 +1,54 @@
+#pragma once
 #ifndef ANN_LMAT_HPP
 #define ANN_LMAT_HPP
 
 #include <vector>
+#include "math_cmp_ann.h"
 
 template <class T>
 class LMat{
 private:
 	unsigned int n_;
-	std::vector<std::vector<T> > mat_;
+	std::vector<T> mat_;
 public:
 	//constructors/destructors
-	LMat():n_(0){};
-	LMat(unsigned int n){resize(n);};
-	LMat(unsigned int n, const T& t){resize(n,t);};
-	~LMat(){};
+	LMat():n_(0){}
+	LMat(unsigned int n){resize(n);}
+	LMat(unsigned int n, const T& t){resize(n,t);}
+	~LMat(){}
 	
 	//access
-	unsigned int& n(){return n_;};
-	const unsigned int& n()const{return n_;};
-	T& operator()(unsigned int i, unsigned int j){return (i>=j)?mat_[i][j]:mat_[j][i];};
-	const T& operator()(unsigned int i, unsigned int j)const{return (i>=j)?mat_[i][j]:mat_[j][i];}
-	T& operator[](unsigned int i);
-	const T& operator[](unsigned int i)const;
+	unsigned int& n(){return n_;}
+	const unsigned int& n()const{return n_;}
+	T& operator()(unsigned int i, unsigned int j);
+	const T& operator()(unsigned int i, unsigned int j)const;
+	T& operator[](unsigned int i){return mat_[i];}
+	const T& operator[](unsigned int i)const{return mat_[i];}
 	
 	//member functions
 	void clear();
-	unsigned int size()const{return (n_*(n_+1))/2;};
-	void resize(unsigned int n);
-	void resize(unsigned int n, const T& t);
+	unsigned int size()const{return (n_*(n_+1))/2;}
+	void resize(unsigned int n){n_=n; mat_.resize(n*(n+1)/2);}
+	void resize(unsigned int n, const T& t){n_=n; mat_.resize(n*(n+1)/2,t);}
 	unsigned int index(unsigned int i, unsigned int j);
 };
 
 //access
 
 template <class T>
-T& LMat<T>::operator[](unsigned int n){
-	for(unsigned int i=0; i<n_; ++i){
-		if(n<mat_[i].size()) return mat_[i][n];
-		else n-=mat_[i].size();
-	}
-	throw std::runtime_error("Invalid lmat index.");
+T& LMat<T>::operator()(unsigned int i, unsigned int j){
+	const unsigned int ii=cmp::min(i,j);
+	const unsigned int jj=cmp::max(i,j);
+	return mat_[ii*(ii+1)/2+jj];
 }
 
 template <class T>
-const T& LMat<T>::operator[](unsigned int n)const{
-	for(unsigned int i=0; i<n_; ++i){
-		if(n<mat_[i].size()) return mat_[n];
-		else n-=mat_[i].size();
-	}
-	throw std::runtime_error("Invalid lmat index.");
+const T& LMat<T>::operator()(unsigned int i, unsigned int j)const{
+	const unsigned int ii=cmp::min(i,j);
+	const unsigned int jj=cmp::max(i,j);
+	return mat_[ii*(ii+1)/2+jj];
 }
-	
+
 //member functions
 
 template <class T>
@@ -60,24 +58,25 @@ void LMat<T>::clear(){
 }
 
 template <class T>
-void LMat<T>::resize(unsigned int n){
-	n_=n; mat_.resize(n_);
-	for(unsigned int i=0; i<n_; ++i) mat_[i].resize(i+1);
-}
-
-template <class T>
-void LMat<T>::resize(unsigned int n, const T& t){
-	n_=n; mat_.resize(n_);
-	for(unsigned int i=0; i<n_; ++i) mat_[i].resize(i+1,t);
-}
-
-template <class T>
 unsigned LMat<T>::index(unsigned int i, unsigned int j){
-	unsigned int ii=i,jj=j;
-	if(i<j){ii=j;jj=i;}
-	unsigned int index=0;
-	for(unsigned int n=0; n<ii; ++n) index+=mat_[n].size();
-	return index+jj;
+	const unsigned int ii=cmp::min(i,j);
+	const unsigned int jj=cmp::max(i,j);
+	return ii*(ii+1)/2+jj;
+}
+
+template <class T> bool operator==(const LMat<T>& lmat1, const LMat<T>& lmat2){
+	if(lmat1.n()!=lmat2.n()) return false;
+	else{
+		unsigned int size=lmat1.size();
+		for(unsigned int i=0; i<size; ++i){
+			if(lmat1[i]!=lmat2[i]) return false;
+		}
+		return true;
+	}
+}
+
+template <class T> inline bool operator!=(const LMat<T>& lmat1, const LMat<T>& lmat2){
+	return !(lmat1==lmat2);
 }
 
 #endif
