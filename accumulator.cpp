55a99869caca6d8501900c937c8456b39d9ce91c
markdellostritto <43013452+markdellostritto@users.noreply.github.com>
2019-12-18 19:26:29 -0500

    made temporary variables const when possible

--- accumulator.cpp
+++ accumulator.cpp
@@ -1,4 +1,18 @@
+//c++ libraries
+#include <iostream>
+#include <stdexcept>
+#include <complex>
+// ann - acuumulator
 #include "accumulator.hpp"
+
+//***********************************************
+//Velocity
+//***********************************************
+
+/*void Vel::push(double pos){
+	if(t_==0) vel_=pos;
+	if(t_==1) vel_=(pos-vel_)/ts_;
+}*/
 
 //***********************************************
 //Distribution
@@ -7,66 +21,115 @@
 //constructors/destructors
 
 Dist::Dist(const Dist& dist){
-	clear();
-	min_=dist.dist_min();
-	max_=dist.dist_max();
+	if(DEBUG_ACC_DIST>0) std::cout<<"Dist::Dost(const Dist&):\n";
+	//init
+	N_=0; M_=0;
+	bc_=0; gc_=0;
+	len_=0; min_=0; max_=0;
+	hist_=NULL;
+	bufx_=NULL;
+	bufy_=NULL;
+	//copy
+	min_=dist.min();
+	max_=dist.max();
 	N_=dist.N();
 	M_=dist.M();
 	bc_=dist.bc();
 	gc_=dist.gc();
-	len_=(max_-min_)/N_;
-	hist_=new double[N_];
-	buf_=new double[M_];
+	if(N_>0) len_=(max_-min_)/N_;
+	if(N_>0) hist_=new double[N_];
+	if(M_>0) bufx_=new double[M_];
+	if(M_>0) bufy_=new double[M_];
 	for(unsigned int i=0; i<N_; ++i) hist_[i]=dist.hist(i);
-	for(unsigned int i=0; i<M_; ++i) buf_[i]=dist.buf(i);
+	for(unsigned int i=0; i<M_; ++i) bufx_[i]=dist.bufx(i);
+	for(unsigned int i=0; i<M_; ++i) bufy_[i]=dist.bufy(i);
 }
 
 Dist::~Dist(){
+	if(DEBUG_ACC_DIST>0) std::cout<<"Dist::~Dist():\n";
 	if(hist_!=NULL) delete[] hist_;
-	if(buf_!=NULL) delete[] buf_;
+	if(bufx_!=NULL) delete[] bufx_;
+	if(bufy_!=NULL) delete[] bufy_;
 }
 
 //operators
 
+bool operator==(const Dist& dist1, const Dist& dist2){
+	return (
+		dist1.N()==dist2.N() &&
+		dist1.len()==dist2.len() &&
+		dist1.min()==dist2.min() && 
+		dist1.max()==dist2.max()
+	);
+}
+
+Dist& Dist::operator+=(const Dist& dist){
+	//check that the dist is compatible
+	if(*this!=dist) throw std::invalid_argument("Incompatible Dist.");
+	//average the two histograms
+	for(unsigned int i=0; i<N_; ++i) hist_[i]=0.5*(hist_[i]+dist.hist(i));
+	//update the global count
+	gc_+=dist.gc();
+	//push the other buffer into own buffer
+	for(unsigned int n=0; n<dist.bc(); ++n) push(dist.bufx(n));
+	for(unsigned int n=0; n<dist.bc(); ++n) push(dist.bufy(n));
+	//return dist
+	return *this;
+}
+
 Dist& Dist::operator=(const Dist& dist){
+	if(DEBUG_ACC_DIST>0) std::cout<<"Dist::operator=(const Dist&):\n";
 	clear();
-	min_=dist.dist_min();
-	max_=dist.dist_max();
+	min_=dist.min();
+	max_=dist.max();
 	N_=dist.N();
 	M_=dist.M();
 	bc_=dist.bc();
 	gc_=dist.gc();
-	len_=(max_-min_)/N_;
-	hist_=new double[N_];
-	buf_=new double[M_];
+	if(N_>0) len_=(max_-min_)/N_;
+	if(N_>0) hist_=new double[N_];
+	if(M_>0) bufx_=new double[M_];
+	if(M_>0) bufy_=new double[M_];
 	for(unsigned int i=0; i<N_; ++i) hist_[i]=dist.hist(i);
-	for(unsigned int i=0; i<M_; ++i) buf_[i]=dist.buf(i);
+	for(unsigned int i=0; i<M_; ++i) bufx_[i]=dist.bufx(i);
+	for(unsigned int i=0; i<M_; ++i) bufy_[i]=dist.bufy(i);
 	return *this;
 }
 
 //member functions
 
 void Dist::clear(){
-	N_=0; M_=1000;
+	if(DEBUG_ACC_DIST>0) std::cout<<"Dist::clear():\n";
+	N_=0; M_=0;
 	bc_=0; gc_=0;
 	len_=0; min_=0; max_=0;
+	norm_=true;
 	if(hist_!=NULL) delete[] hist_; hist_=NULL;
-	if(buf_!=NULL) delete[] buf_; buf_=NULL;
+	if(bufx_!=NULL) delete[] bufx_; bufx_=NULL;
+	if(bufy_!=NULL) delete[] bufy_; bufy_=NULL;
 }
 
-void Dist::init(double min, double max, unsigned int N){
+void Dist::init(double min, double max, unsigned int N, unsigned int M){
+	if(DEBUG_ACC_DIST>0) std::cout<<"Dist::init(double min, double max, unsigned int N, unsigned int M=1):\n";
 	if(min>=max) throw std::runtime_error("Invalid Dist limits.");
 	if(N==0) throw std::runtime_error("Invalid Dist N.");
-	clear();
+	if(M==0) throw std::runtime_error("Invalid Dist M.");
+	if(hist_!=NULL) delete[] hist_; hist_=NULL;
+	if(bufx_!=NULL) delete[] bufx_; bufx_=NULL;
+	if(bufy_!=NULL) delete[] bufy_; bufy_=NULL;
+	bc_=0; gc_=0;
 	min_=min;
 	max_=max;
 	N_=N;
-	len_=(max-min)/N_;
-	hist_=new double[N_];
-	buf_=new double[M_];
+	M_=M;
+	if(N_>0) len_=(max-min)/N_;
+	if(N_>0) hist_=new double[N_];
+	if(M_>0) bufx_=new double[M_];
+	if(M_>0) bufy_=new double[M_];
 }
 
 unsigned int Dist::bin(double x){
+	if(DEBUG_ACC_DIST>0) std::cout<<"Dist::bin(double):\n";
 	unsigned int uLim=N_;
 	unsigned int lLim=0;
 	unsigned int mid;
@@ -79,12 +142,40 @@ unsigned int Dist::bin(double x){
 }
 
 void Dist::push(double x){
-	buf_[bc_++]=x;
+	if(DEBUG_ACC_DIST>0) std::cout<<"Dist::push(double):\n";
+	bufx_[bc_]=x;
+	bufy_[bc_]=1.0;
+	++bc_;
 	if(bc_==M_){
-		for(unsigned int i=0; i<N_; ++i) hist_[i]*=gc_/(gc_+1.0);
-		++gc_;
-		for(unsigned int i=0; i<M_; ++i) hist_[bin(buf_[i])]+=1.0/(M_*gc_);
-		bc_=0;
+		if(norm_){
+			for(unsigned int i=0; i<N_; ++i) hist_[i]*=gc_/(gc_+1.0);
+			++gc_;
+			for(unsigned int i=0; i<M_; ++i) hist_[bin(bufx_[i])]+=bufy_[i]/(M_*gc_);
+			bc_=0;
+		} else {
+			++gc_;
+			for(unsigned int i=0; i<M_; ++i) hist_[bin(bufx_[i])]+=bufy_[i];
+			bc_=0;
+		}
+	}
+}
+
+void Dist::push(double x, double y){
+	if(DEBUG_ACC_DIST>0) std::cout<<"Dist::push(double,double):\n";
+	bufx_[bc_]=x;
+	bufy_[bc_]=y;
+	++bc_;
+	if(bc_==M_){
+		if(norm_){
+			for(unsigned int i=0; i<N_; ++i) hist_[i]*=gc_/(gc_+1.0);
+			++gc_;
+			for(unsigned int i=0; i<M_; ++i) hist_[bin(bufx_[i])]+=bufy_[i]/(M_*gc_);
+			bc_=0;
+		} else {
+			++gc_;
+			for(unsigned int i=0; i<M_; ++i) hist_[bin(bufx_[i])]+=bufy_[i];
+			bc_=0;
+		}
 	}
 }
 
@@ -95,7 +186,31 @@ void Dist::push(double x){
 #ifdef ACC_FFT
 
 //constants
+
 const std::complex<double> Fourier::I=std::complex<double>(0.0,1.0);
+
+//operators
+
+bool operator==(const Fourier& f1, const Fourier& f2){
+	return (f1.N()==f2.N());
+}
+
+Fourier& Fourier::operator+=(const Fourier& fourier){
+	//check compatibility
+	if(*this!=fourier) throw std::invalid_argument("Incompatible Fourier");
+	//average the fourier transforms
+	for(unsigned int n=0; n<N_; ++n){
+		f_[n]=0.5*(f_[n]+fourier.f(n));
+	}
+	//push other buffer onto own buffer
+	for(unsigned int i=0; i<fourier.n(); ++i){
+		push(fourier.fft().in(i));
+	}
+	//return fourier
+	return *this;
+}
+
+//member functions
 
 void Fourier::clear(){
 	fft_.clear();
--- accumulator.hpp
+++ accumulator.hpp
@@ -1,16 +1,13 @@
+#pragma once
 #ifndef ACCUMULATOR_HPP
 #define ACCUMULATOR_HPP
 
-//c libaries
-#include <cstdlib>
+// c libaries
 #include <cmath>
 #include <cfloat>
-//c++ libraries
-#include <stdexcept>
-#include <complex>
-//local - math constants
-#include "math_const.hpp"
-//local - fft
+// c++ libraries
+#include <iosfwd>
+// ann - fft
 #ifdef ACC_FFT
 	#include "fft.hpp"
 #endif
@@ -23,9 +20,9 @@ template <typename... Args>
 class Accumulator1D: public Args...{
 public:
 	//constructors/destructors
-	Accumulator1D():Args()...{};
-	Accumulator1D(const Accumulator1D<Args...>& a):Args(a)...{};
-	~Accumulator1D(){};
+	Accumulator1D():Args()...{}
+	Accumulator1D(const Accumulator1D<Args...>& a):Args(a)...{}
+	~Accumulator1D(){}
 	//operators
 	Accumulator1D<Args...>& operator=(const Accumulator1D<Args...>& a);
 	//members
@@ -54,13 +51,15 @@ private:
 	double min_;
 public:
 	//constructors/destructors
-	Min():min_(FLT_MAX){};
-	~Min(){};
+	Min():min_(FLT_MAX){}
+	~Min(){}
+	//operators
+	Min& operator+=(const Min& min){min_=(min.min()<min_)?min.min():min_;return *this;}
 	//access
-	const double& min(){return min_;};
+	const double& min()const{return min_;}
 	//member functions
-	void clear(){min_=FLT_MAX;};
-	void push(double x){min_=(x<min_)?x:min_;};
+	void clear(){min_=FLT_MAX;}
+	void push(double x){if(x<min_)min_=x;}
 };
 
 //***********************************************
@@ -72,13 +71,15 @@ private:
 	double max_;
 public:
 	//constructors/destructors
-	Max():max_(FLT_MIN){};
-	~Max(){};
+	Max():max_(FLT_MIN){}
+	~Max(){}
+	//operators
+	Max& operator+=(const Max& max){max_=(max.max()>max_)?max.max():max_;return *this;}
 	//access
-	double max(){return max_;};
+	const double& max()const{return max_;}
 	//member functions
-	void clear(){max_=FLT_MIN;};
-	void push(double x){max_=(x>max_)?x:max_;};
+	void clear(){max_=FLT_MIN;}
+	void push(double x){if(x>max_)max_=x;}
 };
 
 //***********************************************
@@ -91,13 +92,15 @@ private:
 	double avg_;
 public:
 	//constructors/destructors
-	Avg():N_(0),avg_(0){};
-	~Avg(){};
+	Avg():N_(0),avg_(0){}
+	~Avg(){}
+	//operators
+	Avg& operator+=(const Avg& avg){avg_+=avg.avg();N_+=avg.N();return *this;}
 	//access
-	const unsigned int& N(){return N_;};
-	double avg(){return avg_;};
+	const unsigned int& N()const{return N_;}
+	double avg()const{return avg_;}
 	//member functions
-	void clear(){N_=0;avg_=0;};
+	void clear(){N_=0;avg_=0;}
 	void push(double x){avg_+=(x-avg_)/(++N_);}
 };
 
@@ -107,62 +110,97 @@ public:
 
 class Var{
 private:
-	unsigned int N_;
-	double avg_;
-	double m2_;
+	unsigned int N_;//total number of datums
+	double avg_;//average of data
+	double m2_;//sum of squares of differences from average
 public:
 	//constructors/destructors
-	Var():N_(0),avg_(0),m2_(0){};
-	~Var(){};
+	Var():N_(0),avg_(0),m2_(0){}
+	~Var(){}
 	//access
-	const unsigned int& N()const{return N_;};
-	double var(){return m2_/(N_-1);};
+	const unsigned int& N()const{return N_;}
+	double var()const{return m2_/(N_-1);}
 	//member functions
 	void clear(){N_=0;avg_=0;m2_=0;};
 	void push(double x){
 		++N_;
-		double delta_=(x-avg_);
+		const double delta_=(x-avg_);
 		avg_+=delta_/N_;
 		m2_+=delta_*(x-avg_);
 	}
 };
 
 //***********************************************
+//Velocity
+//***********************************************
+
+/*class Vel{
+private:
+	unsigned int t_;//total number of datums
+	double ts_;//timestep
+	double vel_;//velocity
+public:
+	//constructors/destructors
+	Vel():t_(0),ts_(1.0),vel_(0.0){};
+	~Vel(){};
+	//access
+	const unsigned int& t()const{return t_;};
+	//member functions
+	void clear(){t_=0;vel_=0;};
+	void push();
+};*/
+
+//***********************************************
 //Distribution
 //***********************************************
 
+#ifndef DEBUG_ACC_DIST
+#define DEBUG_ACC_DIST 0
+#endif 
+
 class Dist{
 private:
-	unsigned int N_,M_,bc_,gc_;
-	double len_,min_,max_;
-	double* hist_;
-	double* buf_;
+	unsigned int N_,M_,bc_,gc_;//nbins,buf size,buf count,global count
+	double len_,min_,max_;//bin len, min, max
+	bool norm_;//whether to normalize histogram
+	double* hist_;//histogram
+	double* bufx_;//buffer - x values
+	double* bufy_;//buffer - y values
 public:
 	//constructors/destructors
-	Dist():N_(0),M_(1000),bc_(0),gc_(0),len_(0),min_(0),max_(0),hist_(NULL),buf_(NULL){};
+	Dist():N_(0),M_(0),bc_(0),gc_(0),len_(0),min_(0),max_(0),norm_(true),hist_(NULL),bufx_(NULL),bufy_(NULL){}
 	Dist(const Dist& dist);
 	~Dist();
 	//operators
 	Dist& operator=(const Dist& dist);
+	Dist& operator+=(const Dist& dist);
 	//access
-	const unsigned int& N()const{return N_;};
-	unsigned int& M(){return M_;};
-	const unsigned int& M()const{return M_;};
-	const unsigned int& bc()const{return bc_;};
-	const unsigned int& gc()const{return gc_;};
-	const double& dist_len()const{return len_;};
-	const double& dist_min()const{return min_;};
-	const double& dist_max()const{return min_;};
-	double hist(unsigned int i)const{return hist_[i];};
-	double buf(unsigned int i)const{return buf_[i];};
-	double abscissa(unsigned int i)const{return min_+len_*i;};
-	double ordinate(unsigned int i)const{return hist_[i];};
+	bool& norm(){return norm_;};
+	const bool& norm()const{return norm_;}
+	const unsigned int& N()const{return N_;}
+	const unsigned int& M()const{return M_;}
+	const unsigned int& bc()const{return bc_;}
+	const unsigned int& gc()const{return gc_;}
+	const double& len()const{return len_;}
+	const double& min()const{return min_;}
+	const double& max()const{return min_;}
+	double hist(unsigned int i)const{return hist_[i];}
+	double bufx(unsigned int i)const{return bufx_[i];}
+	double bufy(unsigned int i)const{return bufy_[i];}
+	double abscissa(unsigned int i)const{return min_+len_*i;}
+	double ordinate(unsigned int i)const{return hist_[i];}
 	//member functions
 	void clear();
-	void init(double min, double max, unsigned int N);
+	void init(double min, double max, unsigned int N, unsigned int M=1);
 	unsigned int bin(double x);
 	void push(double x);
+	void push(double x, double y);
 };
+
+//operators
+
+bool operator==(const Dist& dist1, const Dist& dist2);
+inline bool operator!=(const Dist& dist1, const Dist& dist2){return !(dist1==dist2);};
 
 //***********************************************
 //Fourier
@@ -171,26 +209,32 @@ public:
 #ifdef ACC_FFT
 class Fourier{
 private:
-	unsigned int N_,n_;
-	fourier::FFT_R2C fft_;
-	fftw_complex* f_;
-	static const std::complex<double> I;
+	unsigned int N_,n_;//fourier size, buf count
+	fourier::FFT_R2C fft_;//fft object
+	fftw_complex* f_;//fourier transform
+	static const std::complex<double> I;//sqrt(-1)
 public:
 	//construtors/destructors
-	Fourier():N_(0),n_(0),f_(NULL){};
-	Fourier(unsigned int N){init(N);};
-	~Fourier(){clear();};
+	Fourier():N_(0),n_(0),f_(NULL){}
+	Fourier(unsigned int N){init(N);}
+	~Fourier(){clear();}
+	//operators
+	Fourier& operator+=(const Fourier& fourier);
 	//access
-	const unsigned int& N()const{return N_;};
-	const unsigned int& n()const{return n_;};
-	const fftw_complex& f(unsigned int i)const{return f_[i];};
-	const double fr(unsigned int i)const{return f_[i][0];};
-	const double fi(unsigned int i)const{return f_[i][1];};
+	const unsigned int& N()const{return N_;}
+	const unsigned int& n()const{return n_;}
+	const fourier::FFT_R2C& fft()const{return fft_;}
+	const fftw_complex& f(unsigned int i)const{return f_[i];}
+	const double fr(unsigned int i)const{return f_[i][0];}
+	const double fi(unsigned int i)const{return f_[i][1];}
 	//member functions
 	void clear();
 	void init(unsigned int N);
 	void push(double x);
 };
+//operators
+bool operator==(const Fourier& f1, const Fourier& f2);
+bool operator!=(const Fourier& f1, const Fourier& f2){!(f1==f2);}
 #endif
 
 //***********************************************
@@ -203,9 +247,9 @@ private:
 
 public:
 	//constructors/destructors
-	Accumulator2D():Args()...{};
-	Accumulator2D(const Accumulator2D<Args...>& a):Args(a)...{};
-	~Accumulator2D(){};
+	Accumulator2D():Args()...{}
+	Accumulator2D(const Accumulator2D<Args...>& a):Args(a)...{}
+	~Accumulator2D(){}
 	//operators
 	Accumulator2D<Args...>& operator=(const Accumulator2D<Args...>& a);
 	//members
@@ -237,17 +281,17 @@ private:
 	double covar_;
 public:
 	//constructors/destructors
-	PCorr():N_(0),avgX_(0),avgY_(0),covar_(0){};
-	~PCorr(){};
+	PCorr():N_(0),avgX_(0),avgY_(0),covar_(0){}
+	~PCorr(){}
 	//access
-	const unsigned int& N(){return N_;};
-	double pcorr(){return covar_/std::sqrt(m2X_*m2Y_);};
+	const unsigned int& N(){return N_;}
+	double pcorr(){return covar_/std::sqrt(m2X_*m2Y_);}
 	//member functions
-	void clear(){N_=0;avgX_=0;avgX_=0;covar_=0;};
+	void clear(){N_=0;avgX_=0;avgX_=0;covar_=0;}
 	void push(double x, double y){
 		++N_;
-		double dX_=(x-avgX_);
-		double dY_=(y-avgY_);
+		const double dX_=(x-avgX_);
+		const double dY_=(y-avgY_);
 		avgX_+=dX_/N_;
 		avgY_+=dY_/N_;
 		m2X_+=dX_*(x-avgX_);
@@ -267,16 +311,16 @@ private:
 	double covar_;
 public:
 	//constructors/destructors
-	Covar():N_(0),avgX_(0),avgY_(0),covar_(0){};
-	~Covar(){};
+	Covar():N_(0),avgX_(0),avgY_(0),covar_(0){}
+	~Covar(){}
 	//access
-	const unsigned int& N(){return N_;};
-	double covar(){return covar_/(N_-1);};
+	const unsigned int& N(){return N_;}
+	double covar(){return covar_/(N_-1);}
 	//member functions
-	void clear(){N_=0;avgX_=0;avgX_=0;covar_=0;};
+	void clear(){N_=0;avgX_=0;avgX_=0;covar_=0;}
 	void push(double x, double y){
 		++N_;
-		double dX_=(x-avgX_);
+		const double dX_=(x-avgX_);
 		avgX_+=dX_/N_;
 		avgY_+=(y-avgY_)/N_;
 		covar_+=dX_*(y-avgY_);
@@ -295,19 +339,19 @@ private:
 	double covar_;
 public:
 	//constructors/destructors
-	LinReg():N_(0),avgX_(0),avgY_(0),m2X_(0),m2Y_(0),covar_(0){};
-	~LinReg(){};
+	LinReg():N_(0),avgX_(0),avgY_(0),m2X_(0),m2Y_(0),covar_(0){}
+	~LinReg(){}
 	//access
-	const unsigned int& N(){return N_;};
-	double m(){return covar_/m2X_;};
-	double b(){return avgY_-m()*avgX_;};
-	double r2(){return covar_*covar_/(m2X_*m2Y_);};
+	const unsigned int& N(){return N_;}
+	double m(){return covar_/m2X_;}
+	double b(){return avgY_-m()*avgX_;}
+	double r2(){return covar_*covar_/(m2X_*m2Y_);}
 	//member functions
-	void clear(){N_=0;avgX_=0;avgY_=0;m2X_=0;m2Y_=0;covar_=0;};
+	void clear(){N_=0;avgX_=0;avgY_=0;m2X_=0;m2Y_=0;covar_=0;}
 	void push(double x, double y){
 		++N_;
-		double dX_=(x-avgX_);
-		double dY_=(y-avgY_);
+		const double dX_=(x-avgX_);
+		const double dY_=(y-avgY_);
 		avgX_+=dX_/N_;
 		avgY_+=dY_/N_;
 		m2X_+=dX_*(x-avgX_);
