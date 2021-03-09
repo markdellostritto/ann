b5faaf7f90692273503338f7c250f9f90aef9f6c
markdellostritto <43013452+markdellostritto@users.noreply.github.com>
2021-03-09 17:43:42 -0500

    Updated types

--- src/typedef.hpp
+++ src/typedef.hpp
@@ -4,18 +4,11 @@
 
 #define EIGEN_NO_DEBUG
 
-// c++ libraries
-#include <vector>
 // extern - Eigen
 #include <Eigen/Dense>
 
-typedef unsigned short ushort;
-typedef unsigned int uint;
-typedef std::vector<unsigned int> vuint;
-typedef Eigen::Vector3d vec3d;
-typedef Eigen::VectorXd vecXd;
-typedef Eigen::MatrixXd matXd;
-typedef std::vector<Eigen::VectorXd> VecList;
-typedef std::vector<Eigen::MatrixXd> MatList;
+typedef Eigen::Matrix<double,3,1> Vec3d;
+typedef Eigen::Matrix<double,Eigen::Dynamic,1> VecXd;
+typedef Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic> MatXd;
 
 #endif
